a43dc14c70abbc1f2bd410bd4a46ad42a819f5a7
TimKabakov <kabakovtimofey@mail.ru>
2020-06-18 14:44:04 +0300

    New Lesson

--- GeekBrains_Java_Homework/src/GeekBrains/Main.java
+++ GeekBrains_Java_Homework/src/GeekBrains/Main.java
@@ -1,74 +1,156 @@
 package GeekBrains;
+
 public class Main {
 
     public static void main(String[] args) {
-        byte B = 1;
-        short s = 2;
-        int i = 1;
-        long l = 800L;
-        float f = 1.5f;
-        double bl = 1.765;
-        boolean T = true;
-        boolean F = false;
-        char C = 'a';
-        String S = "sssss";
-        System.out.println("jfhdsj");
-
-        
-        System.out.println(Calculation(34,2,56,32));
-        if (Check(12,14) == true){
+//        ChangeOfVariableIfElse();
+//        ChangeOfVariableSwitch();
+//        VariableInput();
+//        VariableInputAlternative();
+//        VariableMultiplication();
+//        VariableInputDblMassive();
+//        FindMinMax();
+        if(CheckBalance() == true){
             System.out.println("True");
-        }else System.out.println("False");
-        PosorNeg(-143);
-        CheckNegative(-24);
-        _name("Ивиан");
-        IsLeap(1604);
-        IsLeap2(1900);
+        } else {
+            System.out.println("False");
+        }
+//        Task7();
 
 
 
     }
-    public static float Calculation(int a, int b, float c, float d){
-       return a * (b + (c / d));
+    public static void ChangeOfVariableIfElse(){
+        int[] massive1 = new int[] {0,1,1,1,0,0,1,1,0,1,1};
+        for (int i = 0; i < massive1.length; i++) {
+            if (massive1[i] == 0){
+                massive1[i] = 1;
+            } else {
+                massive1[i] = 0;
+            }
+            System.out.println(massive1[i]);
+        }
     }
-    public static boolean Check(int a, int b ){
-        if (a+b>=10 && a+b<=20){
-            return true;
-        } else
-            return false;
+    public static void ChangeOfVariableSwitch() {
+        int[] massive1 = new int[]{0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1};
+        for (int i = 0; i < massive1.length; i++) {
+            switch (massive1[i]) {
+                case 0: {
+                    massive1[i] = 1;
+                    break;
+                }
+                case 1: {
+                    massive1[i] = 0;
+                    break;
+                } default: {
+                    System.out.println("Wrong number");
+                }
+            } System.out.println(massive1[i]);
+        }
     }
-    public static void PosorNeg(int a) {
-        if (a >= 0) {
-            System.out.println("Positive");
-        } else
-            System.out.println("Negative");
+    public static void VariableInput(){
+        int[] massive2 = new int[8];
+        for (int i = 1; i < massive2.length; i++) {
+            massive2[i] = 3 + massive2[i-1];
+        }
     }
-    public static boolean CheckNegative(int a){
-         if(a < 0)
-         return true;
-         else
-         return false;
+    public static void VariableInputAlternative(){
+        int[] massive2 = new int[8];
+        for (int i = 0; i < massive2.length; i++) {
+            if(i == 0){
+                massive2[i] = 0;
+            } else {
+                massive2[i] = 3 + massive2[i-1];
+            }
+            System.out.println(massive2[i]);
+        }
+    }
+    public static void VariableMultiplication() {
+        int[] massive3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < massive3.length; i++) {
+            if (massive3[i] < 6) {
+                massive3[i] = massive3[i] * 2;
+            } else {
+                massive3[i] = massive3[i];
+            }
+            System.out.println(massive3[i]);
+        }
+    }
+    public static void VariableInputDblMassive() {
+        int[][] dblmassive = new int[7][7];
+        for (int i = 0; i < dblmassive.length; i++) {
+            int k = dblmassive.length-i;
+            for (int j = 0; j < dblmassive[i].length; j++) {
+
+                if (i == j) {
+                    dblmassive[i][j] = 1;
+                } else {
+                    dblmassive[i][j] = 0;
+                }
+                dblmassive[i][k-1] = 1;
+                System.out.print(dblmassive[i][j]);
+            }
+
+            System.out.println();
+        }
+    }
+    public static void FindMinMax(){
+        int[] massive4 = new int[]{-1001,2,5,-7,3,55,6,-15,-288,0,144};
+        int min = massive4[0];
+        int max = massive4[0];
+        for (int i = 0; i < massive4.length ; i++) {
+            if(massive4[i] < min){
+                min = massive4[i];
+            } else if(massive4[i] > max){
+                max = massive4[i];
+            }
+        }
+        System.out.println("Min= " + min);
+        System.out.println("Max= " + max);
     }
 
-    public static void _name(String Name) {
-        System.out.println("Привет, " + Name + "!");
+    public static boolean CheckBalance(){
+        int[] massive5 = new int[] {1,2,2,3,8,4,4,4,4};
+        int sum = 0;
+        for (int i = 0; i < massive5.length; i++) {
+            sum = sum + massive5[i];
+        }
+        int Left = 0;
+        int Right = sum;
+        for (int i = 0; i < massive5.length; i++) {
+            Left = Left + massive5[i];
+            Right = Right - massive5[i];
+               if (Left == Right){
+                   return true;
+               }
+        }
+        return false;
     }
 
+    public static void Task7(){
+        int[] massive = new int[]{1,2,3,4,5,6,7,8};
+        int shift = -4;
+        shift = shift %massive.length;
 
-    public static void IsLeap(int year){
-        if (year%400==0)
-            System.out.println("Год високосный");
-        else if (year%100==0)
-            System.out.println("Год не високосный");
-        else if (year%4 ==0)
-            System.out.println("Год високосный");
-        else
-            System.out.println("Год не високосный");
-    }
-    public static void IsLeap2(int year){
-        if (year%400==0 && year%100!=0 && year%4==0)
-            System.out.println("Год високосный");
-        else System.out.println("Год не високосный");
-    }
+        for (int i = 0; i < massive.length; i++) {
+            if (shift == 0){
+                break;
+            } else if(shift <= massive.length/2) {
+                if (i + shift < massive.length) {
+                    int shiftI = (i + shift) % massive.length;
+                    int temp = massive[i];
+                    massive[i] = massive[shiftI];
+                    massive[shiftI] = temp;
+                } else {
+                    break;
+                }
+            } else {
+            }
 
+
+        }
+    }
 }
+
+
+
